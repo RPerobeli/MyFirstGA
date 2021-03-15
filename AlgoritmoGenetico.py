bcb44c0b83f83800f94fb85c8d6fa4dc8fdddb65
RPerobeli <rodrigo.costa2015@engenharia.ufjf.br>
2021-03-15 14:54:34 -0300

    commit inicial

---
+++ AlgoritmoGenetico.py
@@ -0,0 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random as rnd
+
+class AlgoritmoGenetico:
+    #atributos
+    DominioVariaveisInicio =0
+    DominioVariaveisFim = 0
+
+    populacaoGenotipo = []
+    populacaoFenotipo = []
+    reprodutores = []
+
+    numReprodutores = 0
+    tamCromossomo = 0
+    def __init__(self, xi, xf, numrep, tamCr, probMut):
+        self.DominioVariaveisInicio = list(xi)
+        self.DominioVariaveisFim = list(xf)
+
+        self.numReprodutores = numrep
+        self.tamCromossomo = tamCr
+
+        self.probMutacao = probMut
+    #endfunc
+
+    def CriaPopulacaoBinaria(self):
+        for i in range(0,self.numReprodutores):
+            individuo = []
+            for i in range(0,len(self.DominioVariaveisInicio)):
+                individuoGene = []
+                for j in range(0,self.tamCromossomo):
+                    individuoGene.append(rnd.randint(0,1))
+                #endfor
+                individuo.append(individuoGene)
+            #endfor
+            self.reprodutores.append(individuo)
+        #endfor
+        self.Reproducao()
+    #endfunc
+
+    def Reproducao(self):
+        self.populacaoGenotipo = []
+        for i in range(0,len(self.reprodutores)):
+            for j in range(i+1,len(self.reprodutores)):
+                filho1, filho2 = self.CrossOver(self.reprodutores[i], self.reprodutores[j])
+                self.populacaoGenotipo.append(filho1)
+                self.populacaoGenotipo.append(filho2)
+            #endfor
+        #endfor
+        self.populacaoGenotipo = np.array(self.populacaoGenotipo)
+    #endfunc
+
+    def CrossOver(self, pai1, pai2):
+        pontoXover = rnd.randint(0,len(pai1))
+        for j in range(0,len(pai1)):
+            for i in range(0,pontoXover):
+                p1 = pai1[j]
+                p2 = pai2[j]
+                p1 = np.insert(p1,i,p2[i])
+                p2 = np.insert(p2,i,p1[i+1])
+                p1 = np.delete(p1,i+1)
+                p2 = np.delete(p2,i+1)
+            #endfor
+        #endfor
+        return np.array(pai1),np.array(pai2)
+    #endfunc
+
+    def Fenotipifica(self, xi, xf):
+        self.populacaoFenotipo = []
+        for individuo in list(self.populacaoGenotipo):
+            individuoFenotipo = []
+            for i in range(0, len(xi)):
+                resultado = xi[i] + self.IND(individuo[i]) * (xf[i]-xi[i])/(2**(len(individuo[i]))-1)
+                individuoFenotipo.append(resultado)
+            #endfor
+            self.populacaoFenotipo.append(individuoFenotipo)
+        #endfor
+    #endfunc
+
+    def IND(self,i):
+        tamanhoCadeiaBits = range(0,len(i))
+        ind = 0
+        for j in tamanhoCadeiaBits:
+            ind += i[j]*2**(len(i)-1-j)
+        #endfor
+        return ind
+    #endfunc
+
+    def SortPopulacoes(self, vetFit):
+        self.populacaoGenotipo = list(self.populacaoGenotipo)
+        for i in range(0,len(vetFit)):
+            for j in range(i,len(vetFit)):
+                if(vetFit[j] > vetFit[i]):
+                    vetFit = np.insert(vetFit,i,vetFit[j])
+                    vetFit = np.delete(vetFit,j+1)
+                    
+                    self.populacaoGenotipo.insert(i,self.populacaoGenotipo[j])
+                    self.populacaoGenotipo.pop(j+1)
+                #endif
+            #endfor
+        #endfor
+        self.populacaoGenotipo = np.array(self.populacaoGenotipo)
+        return vetFit
+    #endfunc
+
+    def SetReprodutores(self):
+        self.reprodutores = []
+        for i in range(0,self.numReprodutores):
+            self.reprodutores.append(self.populacaoGenotipo[i])
+        #endfor
+        #self.reprodutores = np.array(self.reprodutores)
+    #endfunc
+
+    def Mutacao(self):
+        for ind in self.populacaoGenotipo:
+            for i in range(0,len(ind)):
+                for j in range(0,len(ind[i])):
+                    if(rnd.uniform(0,1) <= self.probMutacao):
+                        if(ind[i][j] == 0):
+                            ind[i][j]=1
+                        else:
+                            ind[i][j]=0
+                        #endif
+                    #endif
+                #endfor
+            #endfor
+        #endfor
+    #endfunc
+
+    def PenalizacaoPorMetade(self, Fit, n):
+        Fit = Fit/(2**n)
+        return Fit
+    #endfunc
+
+    def PenalizacaoPorDobro(self, Fit, n):
+        Fit = Fit*(2**n)
+        return Fit
+    #endfunc
+
+#endclass
